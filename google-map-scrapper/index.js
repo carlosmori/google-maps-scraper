b5d7248562eb7d7a2f34a834dbec1b408fb7ba60
Carlos Mori <cmori.94@gmail.com>
2024-04-06 19:42:49 +0700

    australian-farms

--- google-map-scrapper/index.js
+++ google-map-scrapper/index.js
@@ -6,14 +6,18 @@ const os = require("os");
 const homeDir = os.homedir();
 const dropboxDir = "Dropbox";
 
-// Function to save CSV data to file
+puppeteerExtra.use(StealthPlugin());
+
+async function launchBrowser() {
+  return await puppeteerExtra.launch({
+    headless: false,
+    args: ["--no-sandbox"],
+  });
+}
+
 function saveCsvToFile(csvData, filePath) {
   fs.writeFileSync(filePath, csvData);
 }
-
-// const ExcelJS = require("exceljs");
-
-puppeteerExtra.use(StealthPlugin());
 
 async function scrollToBottom(page) {
   const wrapper = await page.$('div[role="feed"]');
@@ -47,19 +51,22 @@ async function scrollToBottom(page) {
     }
   }
 }
+async function waitForPageLoad(page) {
+  // Wait until the 'load' event is fired, indicating that the page has finished loading
+  await page.waitForNavigation({ waitUntil: "domcontentloaded" });
 
-async function scrapeGoogleMaps(query) {
-  const browser = await puppeteerExtra.launch({
-    headless: false,
-    args: ["--no-sandbox"],
-  });
+  // Alternatively, you can wait for a specific element to appear on the page
+  // Example: await page.waitForSelector('your-selector');
+}
+
+async function scrapeGoogleMaps(page, query) {
   console.log("starting scrapper!");
-  const page = await browser.newPage();
   try {
     await page.goto(
       `https://www.google.com/maps/search/${query.split(" ").join("+")}`,
       { timeout: 1200000 }
     );
+    await waitForPageLoad(page);
     await scrollToBottom(page);
 
     const html = await page.content();
@@ -94,31 +101,23 @@ async function scrapeGoogleMaps(query) {
       })
       .get();
 
-    await browser.close();
     console.log(`scrapped ${businesses.length} !`);
     return businesses;
   } catch (error) {
     console.error("Error:", error.message);
-    await browser.close();
     return [];
   }
 }
 
-async function scrapIndividualResult(scrapIndividualResult) {
-  const browser = await puppeteerExtra.launch({
-    headless: false,
-    args: ["--no-sandbox"],
-  });
-  // executablePath:
-  //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", // Ruta al ejecutable de Chrome en tu sistema
-  const page = await browser.newPage();
+async function scrapIndividualResult(page, scrapIndividualResult) {
   try {
     await page.goto(scrapIndividualResult.googleUrl, { timeout: 60000 });
+    await waitForPageLoad(page);
+
     const html = await page.content();
     const $ = cheerio.load(html);
     const websiteLink = $('a[data-item-id="authority"]').attr("href");
     const phoneNumber = $('[aria-label*="Phone"]').text();
-    await browser.close();
     return {
       ...scrapIndividualResult,
       websiteLink,
@@ -126,7 +125,6 @@ async function scrapIndividualResult(scrapIndividualResult) {
     };
   } catch (error) {
     console.error("Error:", error.message);
-    await browser.close();
     return [];
   }
 }
@@ -137,102 +135,68 @@ async function delay(ms) {
 
 (async () => {
   const potentialFarms = [
-    "Strawberry Farm",
-    "Grape Farm",
     "Cattle Farm",
-    "Dairy Farm",
     "Sheep Farm",
-    "Poultry Farm",
-    "Aquaculture Farm",
-    "Vegetable Farm",
-    "Grain Farm",
-    "Nut Farm",
-    "Olive Farm",
-    "Tea Plantation",
-    "Coffee Plantation",
-    "Soybean Farm",
-    "Corn Farm",
-    "Rice Farm",
     "Wheat Farm",
     "Barley Farm",
     "Canola Farm",
+    "Rice Farm",
+    "Sugar Cane Farm",
+    "Cotton Farm",
+    "Citrus Farm",
+    "Banana Farm",
+    "Avocado Farm",
+    "Mango Farm",
+    "Apple Farm",
+    "Pear Farm",
+    "Grape Farm",
     "Almond Farm",
-    "Hazelnut Farm",
+    "Walnut Farm",
     "Pistachio Farm",
     "Macadamia Farm",
-    "Cashew Farm",
-    "Peanut Farm",
-    "Sunflower Farm",
-    "Cotton Farm",
-    // "Sugarcane Farm",
-    // "Banana Farm",
-    // "Pineapple Farm",
-    // "Mango Farm",
-    // "Avocado Farm",
-    // "Citrus Farm",
-    // "Apple Farm",
-    // "Pear Farm",
-    // "Cherry Farm",
-    // "Blueberry Farm",
-    // "Raspberry Farm",
-    // "Blackberry Farm",
-    // "Cranberry Farm",
-    // "Gooseberry Farm",
-    // "Honey Farm",
-    // "Fish Farm",
-    // "Shrimp Farm",
-    // "Salmon Farm",
-    // "Trout Farm",
-    // "Tilapia Farm",
-    // "Catfish Farm",
-    // "Oyster Farm",
-    // "Clam Farm",
-    // "Mussel Farm",
-    // "Lobster Farm",
-    // "Crab Farm",
-    // "Scallop Farm",
-    // "Abalone Farm",
-    // "Seaweed Farm",
-    // "Herb Farm",
-    // "Flower Farm",
-    // "Bonsai Farm",
-    // "Nursery",
-    // "Hydroponic Farm",
-    // "Mushroom Farm",
-    // "Alpaca Farm",
-    // "Emu Farm",
-    // "Kangaroo Farm",
-    // "Crocodile Farm",
-    // "Worm Farm",
-    // "Snail Farm",
-    // "Bee Farm",
-    // "Silkworm Farm",
-    // "Algae Farm",
-    // "Frog Farm",
-    // "Game Farm",
-    // "Deer Farm",
-    // "Buffalo Farm",
-    // "Goat Farm",
-    // "Rabbit Farm",
-    // "Pheasant Farm",
-    // "Quail Farm",
-    // "Duck Farm",
-    // "Turkey Farm",
-    // "Guinea Fowl Farm",
-    // "Pigeon Farm",
-    // "Bison Farm",
-    // "Llama Farm",
-    // "Yak Farm",
-    // "Horse Farm",
-    // "Donkey Farm",
-    // "Camel Farm",
-    // "Elephant Farm",
-    // "Exotic Animal Farm",
-    // "Petting Zoo",
+    "Olive Farm",
+    "Poultry Farm",
+    "Pig Farm",
+    "Fish Farm",
+    "Aquaculture Farm",
+    "Horticulture Farm",
+    "Vegetable Farm",
+    "Potato Farm",
+    "Tomato Farm",
+    "Carrot Farm",
+    "Onion Farm",
+    "Lettuce Farm",
+    "Broccoli Farm",
+    "Cauliflower Farm",
+    "Spinach Farm",
+    "Strawberry Farm",
+    "Raspberry Farm",
+    "Blueberry Farm",
+    "Coffee Farm",
+    "Tea Farm",
+    "Herb Farm",
+    "Flower Farm",
+    "Nursery Farm",
+    "Seaweed Farm",
+    "Algae Farm",
+    "Solar Farm",
+    "Wind Farm",
+    "Hydroponic Farm",
+    "Hydroelectric Farm",
+    "Organic Farm",
+    "Permaculture Farm",
+    "Agroforestry Farm",
+    "Bush Tucker Farm",
+    "Dairy Farm",
+    "Egg Farm",
+    "Honey Farm",
+    "Bee Farm",
+    "Mushroom Farm",
+    "Ranch",
   ];
-  const states = [
-    "New South Wales",
-    "Queensland",
+  const australianStates = [
+    // "New South Wales",
+    // "Queensland",
     "South Australia",
     "Tasmania",
     "Victoria",
@@ -243,27 +207,44 @@ async function delay(ms) {
 
   const combinations = [];
   for (const potentialFarm of potentialFarms) {
-    for (const state of states) {
+    for (const state of australianStates) {
       const query = `${potentialFarm} in ${state}`;
       combinations.push(query);
     }
   }
   try {
-    for (const query of combinations) {
-      const googleMapQuery = query; // Concatena todos los argumentos después del nombre del script
-      const csvFileName = googleMapQuery.replace(/\s+/g, "-");
-      console.log(googleMapQuery);
-      const businesses = await scrapeGoogleMaps(googleMapQuery);
+    const browser = await launchBrowser();
+    const page = await browser.newPage();
+    for (const state of australianStates) {
       const finalBusinesses = [];
-      const provBs = [businesses[0], businesses[1], businesses[2]];
-      for (const element of provBs) {
-        console.log(`scrapping individual bussines!!`);
-        const resultWithAdditionalData = await scrapIndividualResult(element);
-        finalBusinesses.push(resultWithAdditionalData);
-        // Delay for 2 seconds before proceeding to the next iteration
-        await delay(1000); // Adjust the delay time as needed
+
+      // Iterate over potential farms
+      for (const farm of potentialFarms) {
+        const googleMapQuery = `${farm} in ${state}`;
+        console.log(`Scraping ${googleMapQuery}...`);
+        const businesses = await scrapeGoogleMaps(page, googleMapQuery);
+
+        // Scrap individual results for each business
+
+        // const provBs = [businesses[0], businesses[1], businesses[2]];
+        const trimbusinesses = businesses.splice(0, 20);
+        for (const business of trimbusinesses) {
+          console.log(
+            `Scraping ${googleMapQuery} for particular result: ${business.storeName}...`
+          );
+          process.stdout.write(
+            `Scraping ${googleMapQuery} for particular result: ${business.storeName}...`
+          );
+          const resultWithAdditionalData = await scrapIndividualResult(
+            page,
+            business
+          );
+          finalBusinesses.push(resultWithAdditionalData);
+
+          // Delay for 2 seconds before proceeding to the next iteration
+          await delay(2000);
+        }
       }
-      // createExcel(finalBusinesses, "Carpinteria-metalica-Granada.xlsx");
       console.log(finalBusinesses);
       // Convert finalBusinesses to CSV format
       let csvContent = `\"Store Name\",\"Website Link\",\"Business Website\",\"Phone Number\",\"Stars\",\"Number of Reviews\",\"Rating Text\",\"Address\",\"Place ID\",\"Category\",\"Google URL\"\n`;
@@ -301,8 +282,12 @@ async function delay(ms) {
 
         csvContent += `${storeName},${websiteLink},${bizWebsite},${phoneNumber},${stars},${numberOfReviews},${ratingText},${address},${placeId},${category},${googleUrl}\n`;
       });
+      const csvFileName = `${state}.csv`;
       const dropboxPath = `${homeDir}/${dropboxDir}`;
-      const csvFilePath = `${dropboxPath}/${csvFileName}.csv`;
+      const csvFilePath = `${dropboxPath}/${csvFileName}`;
+      saveCsvToFile(csvContent, csvFilePath);
+
+      console.log(`CSV file for ${state} saved successfully.`);
 
       saveCsvToFile(csvContent, csvFilePath);
 
@@ -310,5 +295,8 @@ async function delay(ms) {
     }
   } catch (error) {
     console.error("Error:", error.message);
+  } finally {
+    await browser.close(); // Close the browser after all operations are complete
+    console.log("Scraping finished!");
   }
 })();
